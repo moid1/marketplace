f3429fc4881dafa0c1037c7281216b0da74e7ecf
Muhammad Aleem <Muhammad.Aleem@chalhoub.com>
2022-04-27 02:56:27 +0500

    change local url

--- platform/plugins/ecommerce/resources/views/orders/thank-you/customer-info.blade.php
+++ platform/plugins/ecommerce/resources/views/orders/thank-you/customer-info.blade.php
@@ -58,9 +58,9 @@
                 "price_amount": order.amount,
                 "price_currency": order.payment.currency,
                 "order_id": $('.order-code').find('span').text().trim(),
-                "ipn_callback_url": "http://marketplace.test/api/payment/"+order.payment.id,
-                "success_url": "http://marketplace.test/customer/orders/view/"+order.id,
-                "cancel_url": "http://marketplace.test/cancel"
+                "ipn_callback_url":window.location.origin + "/api/payment/"+order.payment.id,
+                "success_url": window.location.origin + "/customer/orders/view/"+order.id,
+                "cancel_url": window.location.origin
             }),
         };
 
